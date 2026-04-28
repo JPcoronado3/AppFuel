f9ccc234796dbfa7c9e88c6d2db765698e08a019
juandiegogalindo <jdgalindo6@gmail.com>
2026-04-28 17:45:36 -0500

    Nuevas estaciones de servicio

--- app/src/main/java/co/edu/unipiloto/scrumbacklog/database/DatabaseHelper.java
+++ app/src/main/java/co/edu/unipiloto/scrumbacklog/database/DatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "app.db";
-    private static final int DATABASE_VERSION = 27; // 🔥 SUBE OTRA VEZ
+    private static final int DATABASE_VERSION = 28; // 🔥 SUBE OTRA VEZ
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -117,6 +117,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS precio_combustible");
         db.execSQL("DROP TABLE IF EXISTS ubicacion");
         db.execSQL("DROP TABLE IF EXISTS combustible");
+        db.execSQL("DROP TABLE IF EXISTS alerta");
 
         onCreate(db);
     }
@@ -129,11 +130,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("INSERT INTO combustible (nombre) VALUES ('Diesel')");
 
         // Estaciones (SIN ID manual)
-        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Suba','Bogota','Suba','06:00','22:00')");
-        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Engativa','Bogota','Engativa','05:00','23:00')");
-        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Centro','Bogota','Centro','07:00','21:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Suba','Bogota','Suba','00:00','22:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Calle 80','Bogota','Calle 80','00:00','23:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Norte','Bogota','Norte','00:00','23:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Americas','Bogota','Americas','00:00','23:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Dorado','Bogota','Dorado','00:00','23:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación 20 de Julio','Bogota','20 de Julio','00:00','21:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Tunal','Bogota','Tunal','00:00','23:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Usme','Bogota','Usme','00:00','23:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Sur','Bogota','Sur','00:00','23:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Caracas','Bogota','Caracas','00:00','23:00')");
+        db.execSQL("INSERT INTO ubicacion (nombre, ciudad, localidad, hora_apertura, hora_cierre) VALUES ('Estación Centro','Bogota','Centro','00:00','23:00')");
 
-        for (int u = 1; u <= 3; u++){
+
+        for (int u = 1; u <= 11; u++){
             db.execSQL("INSERT INTO inventario VALUES (NULL,1,"+u+",10000)");
             db.execSQL("INSERT INTO inventario VALUES (NULL,2,"+u+",8000)");
             db.execSQL("INSERT INTO inventario VALUES (NULL,3,"+u+",7500)");
@@ -147,5 +157,29 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("INSERT INTO precio_combustible VALUES (NULL,1,3,15800)");
         db.execSQL("INSERT INTO precio_combustible VALUES (NULL,2,3,22500)");
         db.execSQL("INSERT INTO precio_combustible VALUES (NULL,3,3,13000)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,1,4,15800)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,2,4,21500)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,3,4,13500)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,1,5,16800)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,2,5,22500)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,3,5,13900)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,1,6,13800)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,2,6,22500)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,3,6,15000)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,1,7,18800)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,2,7,20500)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,3,7,13000)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,1,8,15800)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,2,8,22900)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,3,8,14800)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,1,9,15800)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,2,9,24700)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,3,9,14650)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,1,10,13800)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,2,10,20500)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,3,10,14570)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,1,11,13800)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,2,11,25500)");
+        db.execSQL("INSERT INTO precio_combustible VALUES (NULL,3,11,13999)");
     }
 }
